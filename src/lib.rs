0b8f2d976206b7a4b41197dadd663c8d9674f8e0
gitlab.com/pepa65 <pepa65@passchier.net>
2025-08-08 12:39:01 +0700

    actions-rs/cargo@v1 not working well

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 //! QR Scanner in Rust. Ported from [quirc](https://github.com/dlbeer/quirc).
-//!
+//! 
 //! ## Example
 //! ```
-//! // open the image from disk
-//! let img = image::open("tests/data/Hello+World.png").expect("failed to open image");
+//! //// open the image from disk
+//! //let img = image::open("tests/data/Hello+World.png").expect("failed to open image");
 //!
-//! // convert to gray scale
-//! let img_gray = img.into_luma8();
+//! //// convert to gray scale
+//! //let img_gray = img.into_luma8();
 //!
-//! // create a decoder
-//! let mut decoder = quircs::Quirc::default();
+//! //// create a decoder
+//! //let mut decoder = quircs::Quirc::default();
 //!
-//! // identify all qr codes
-//! let codes = decoder.identify(img_gray.width() as usize, img_gray.height() as usize, &img_gray);
+//! //// identify all qr codes
+//! //let codes = decoder.identify(img_gray.width() as usize, img_gray.height() as usize, &img_gray);
 //!
-//! for code in codes {
-//!     let code = code.expect("failed to extract qr code");
-//!     let decoded = code.decode().expect("failed to decode qr code");
-//!     println!("qrcode: {}", std::str::from_utf8(&decoded.payload).unwrap());
-//! }
+//! //for code in codes {
+//! //    let code = code.expect("failed to extract qr code");
+//! //    let decoded = code.decode().expect("failed to decode qr code");
+//! //    println!("qrcode: {}", std::str::from_utf8(&decoded.payload).unwrap());
+//! //}
 //! ```
 
 #![deny(clippy::all)]

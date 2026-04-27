7c81422a4a2881123a083d4d66967e483940e8d8
gitlab.com/pepa65 <pepa65@passchier.net>
2026-04-27 09:06:12 +0700

    Version check

--- src/identify.rs
+++ src/identify.rs
@@ -613,7 +613,9 @@ fn measure_timing_pattern(qr: &mut Grid, capstones: &[Capstone], image: &Image<'
 	let size = scan * 2 + 13;
 	let ver = (size - 15) / 4;
 	qr.grid_size = ver * 4 + 17;
-
+	if !(VERSION_MIN..=VERSION_MAX).contains(&(ver as usize)) {
+		return -1;
+	}
 	0
 }
 
@@ -700,12 +702,21 @@ fn fitness_capstone(qr: &Grid, image: &Image<'_>, mut x: i32, mut y: i32) -> i32
 
 const MAX_ALIGNMENT: usize = 7;
 
+fn get_version_info(version: usize) -> Option<&'static VersionInfo> {
+	if version >= VERSION_DB.len() {
+		return None;
+	}
+	Some(&VERSION_DB[version])
+}
+
 /// Compute a fitness score for the currently configured perspective transform,
 /// using the features we expect to find by scanning the grid.
 fn fitness_all(qr: &Grid, image: &Image<'_>) -> i32 {
 	let version = usize::try_from((qr.grid_size - 17) / 4).expect("invalid version");
-	let info = &VERSION_DB[version];
 	let mut score: i32 = 0;
+	let Some(info) = get_version_info(version) else {
+		return score;
+	};
 
 	// Check the timing pattern
 	for i in 0..qr.grid_size - 14 {
@@ -718,9 +729,6 @@ fn fitness_all(qr: &Grid, image: &Image<'_>) -> i32 {
 	score += fitness_capstone(qr, image, 0, 0);
 	score += fitness_capstone(qr, image, qr.grid_size - 7, 0);
 	score += fitness_capstone(qr, image, 0, qr.grid_size - 7);
-	if version > VERSION_MAX {
-		return score;
-	}
 
 	// Check alignment patterns
 	let mut ap_count = 0;
